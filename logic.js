93474e60273091d94f2008e93f30871f1a3fc575
Wei Zhe <weizhe.chong@hextar.com>
2026-02-07 12:20:51 +0800

    Create variables for numbers and operator and operate function

--- logic.js
+++ logic.js
@@ -13,3 +13,23 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let num1;
+let operator;
+let num2;
+
+function operate(num1, operator, num2) {
+    const result = 0;
+    if (operator === '+') {
+        result = add(num1, num2);
+    } else if (operator === '-') {
+        result = subtract(num1, num2);
+    } else if (operator === '*') {
+        result = multiply(num1, num2);
+    } else if (operator === '/') {
+        result = divide(num1, num2);
+    }
+
+    return result;
+}
+
